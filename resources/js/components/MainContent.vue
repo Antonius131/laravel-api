168f0bb419d448551bff8d3e431dbb15baac9b80
Antonius131 <antomendetta@gmail.com>
2022-09-27 16:36:32 +0200

    creating MainContent template structure

--- resources/js/components/MainContent.vue
+++ resources/js/components/MainContent.vue
@@ -1,5 +1,13 @@
 <template>
-
+   <main>
+      <div class="container">
+         <div class="row py-5">
+            <div class="col-12">
+               <h2>Main Content</h2>
+            </div>
+         </div>
+      </div>
+   </main>
 </template>
 
 <script>

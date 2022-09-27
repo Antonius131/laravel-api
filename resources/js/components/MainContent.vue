ecc6afa67ba45d97aa5368f9a9d4ef3506824672
Antonius131 <antomendetta@gmail.com>
2022-09-27 16:41:55 +0200

    importing component in MainContent

--- resources/js/components/MainContent.vue
+++ resources/js/components/MainContent.vue
@@ -4,6 +4,7 @@
          <div class="row py-5">
             <div class="col-12">
                <h2>Main Content</h2>
+               <PostCard />
             </div>
          </div>
       </div>
@@ -11,8 +12,12 @@
 </template>
 
 <script>
-   export default {
+   import PostCard from './PostCard.vue';
 
+   export default {
+      components: {
+         PostCard
+      }
    }
 </script>
 
